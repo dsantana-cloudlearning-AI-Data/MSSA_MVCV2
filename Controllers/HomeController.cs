f36d44447802aad8e454a525c8178e36f0cc911f
DavidS <david3651@live.com>
2021-12-13 18:26:03 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EventPlannerV2._5.Models;
+
+namespace EventPlannerV2._5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DataContext context;
+        public HomeController(DataContext ctx) => context = ctx;
+        public IActionResult Index() => View();
+        public IActionResult Respond() => View();
+
+        [HttpPost]
+        public IActionResult Respond(GuestResponse response)
+        {
+            context.Responses.Add(response);
+            context.SaveChanges();
+            return
+            RedirectToAction(nameof(Thanks), new { Name = response.Name, WillAttend = response.WillAttend });
+        }
+
+        public IActionResult Thanks(GuestResponse response) => View(response);
+        public IActionResult ListResponses() => View(context.Responses.OrderByDescending(r => r.WillAttend));
+    }
+}
---
+++ Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventPlannerV2._5.Models
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options) : base(options) { }
+
+        public DbSet<GuestResponse> Responses { get; set; }
+    }
+}
